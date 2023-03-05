9cbc67c62be6a4ff79bd1c81f2fbe57718fba90c
Matt <ethacktry@gmail.com>
2023-03-05 14:19:10 +0100

    Finished exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        console.log(year % 4);
+        console.log(year % 100);
+        console.log(year % 400);
+        return true;
+    } else if (year % 100 === 0 && year % 400 === 0 ) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+leapYears(1600);
+
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // f - 32 / 1.8
+  let result = (temp - 32) / 1.8;
+  result = Number(result.toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  //c * 1.8 + 32
+  let result = temp * 1.8 + 32;
+  result = Number(result.toFixed(1));
+  return result;
 };
+
+convertToCelsius(-100)
+convertToFahrenheit(73.2)
 
 // Do not edit below this line
 module.exports = {
