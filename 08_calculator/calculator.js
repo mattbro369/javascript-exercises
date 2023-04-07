31de07e7961219ecdef6070cea32f954abd7b836
matt <ethacktry@gmail.com>
2023-04-07 12:42:41 +0200

    Finished exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,42 @@
-const add = function() {
-	
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const sum = function (array) {
+	return array.reduce(
+		(accumulator, currentValue) => accumulator + currentValue,
+		0
+	);
 };
 
-const subtract = function() {
-	
+const multiply = function (array) {
+	return array.reduce(
+		(accumulator, currentValue) => accumulator * currentValue
+	);
 };
 
-const sum = function() {
-	
-};
+const power = (num, power) => num ** power;
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+	if (num === 0) {
+		return 1;
+	} else {
+		const arr = [num];
+		for (i = num - 1; i > 0; i--) {
+			arr.push(i);
+		}
+		return arr.reduce(
+			(accumulator, currentValue) => accumulator * currentValue
+		);
+	}
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
