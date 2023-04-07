42bb221764429dc9b251a81dac5b907dbff83930
matt <ethacktry@gmail.com>
2023-04-07 17:57:09 +0200

    Finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (word) {
+	const arr = Array.from(word.toLowerCase().replace(/[^a-z]/g, ""));
 
+	const arrReverse = [...arr].reverse();
+
+	return arr.every((char, i) => char === arrReverse[i]);
 };
 
 // Do not edit below this line
