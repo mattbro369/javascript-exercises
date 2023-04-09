3f20153be8968e14fb98c3a29bc0dc359a0d82fc
matt <ethacktry@gmail.com>
2023-04-09 14:41:40 +0200

    Finished exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+	const result = array.map((array) => array.title);
+	return result;
 };
+
+const books = [
+	{
+		title: "Book",
+		author: "Name",
+	},
+	{
+		title: "Book2",
+		author: "Name2",
+	},
+];
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
